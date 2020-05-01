e28c9fe887fbb3a9e7da246984d13bf19116b87d
hare451g <hendrasadewa30@gmail.com>
2020-05-01 16:56:39 +0700

    add store provider to Project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { StoreProvider } from 'easy-peasy';
+
 import './index.css';
+
 import App from './App';
+import store from './App/store';
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <StoreProvider store={store}>
+      <App />
+    </StoreProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
