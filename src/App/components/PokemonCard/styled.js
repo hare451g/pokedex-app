eab9b5959bb5f04647bc3a69adfc6c11cf61c12f
hare451g <hendrasadewa30@gmail.com>
2020-05-02 05:12:10 +0700

    styling link for pokemon card

---
+++ src/App/components/PokemonCard/styled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import { Link as BaseLink } from '@reach/router';
+
+const Link = styled(BaseLink)`
+  text-decoration: none;
+  color: inherit;
+`;
+
+export { Link };
