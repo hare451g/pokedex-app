014fa9c8aa376d6c1095b48114827382345cf570
hare451g <hendrasadewa30@gmail.com>
2020-05-02 01:42:05 +0700

    create pokemon deck container

---
+++ src/App/containers/PokemonDeck/index.js
@@ -0,0 +1,52 @@
+import React, { useEffect } from 'react';
+import { useStoreActions, useStoreState } from 'easy-peasy';
+import PokemonCard from '../../components/PokemonCard';
+import { Box, InfiniteScroll } from 'grommet';
+
+function PokemonDeck() {
+  const { isLoading, error, count, next, results } = useStoreState(
+    (state) => state.pokemonList
+  );
+
+  const { fetchPokemonThunk } = useStoreActions(
+    (actions) => actions.pokemonList
+  );
+
+  const onMore = () => {
+    console.log('moar', next, count);
+    if (next) {
+      fetchPokemonThunk({ nextUrl: next });
+    }
+  };
+
+  useEffect(() => {
+    fetchPokemonThunk({ limit: 10, offset: 0 });
+  }, [fetchPokemonThunk]);
+
+  return (
+    <Box
+      direction="row"
+      overflow="auto"
+      margin="small"
+      justify="center"
+      align="center"
+      wrap
+    >
+      {error ? (
+        <div>
+          <h5>An error occurred</h5>
+          <span>{error}</span>
+        </div>
+      ) : (
+        results.length > 0 && (
+          <InfiniteScroll items={results} step={10} onMore={onMore}>
+            {(pokemon) => <PokemonCard {...pokemon} key={pokemon.id} />}
+          </InfiniteScroll>
+        )
+      )}
+      {isLoading && <span>Loading contents . . .</span>}
+    </Box>
+  );
+}
+
+export default PokemonDeck;
