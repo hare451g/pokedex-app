bba83433cf58ad5b56102e726b718732d3a1150b
hare451g <hendrasadewa30@gmail.com>
2020-05-02 04:21:19 +0700

    remove meter

--- src/App/components/PokemonStats/index.js
+++ src/App/components/PokemonStats/index.js
@@ -1,23 +1,12 @@
 import React from 'react';
-import { Box, Meter } from 'grommet';
+import { Box } from 'grommet';
 
 function PokemonStats({ base_stat: baseStat, effort, stat }) {
   return (
-    <Box direction="column" justify="between" align="center">
+    <Box direction="column" justify="between" align="center" pad="small">
       <Box direction="row" justify="between" align="center" fill>
         <div>{stat.name}</div>
         <div>{baseStat}</div>
-      </Box>
-      <Box background="#fefefe" fill>
-        <Meter
-          values={[
-            {
-              value: baseStat,
-              onClick: () => {},
-            },
-          ]}
-          aria-label="meter"
-        />
       </Box>
     </Box>
   );
