ffa41610ee45e295df378ac32e8a13a1a0e92cbd
hare451g <hendrasadewa30@gmail.com>
2020-05-01 16:17:05 +0700

    create fetch pokemon list function to fetch data from api

---
+++ src/App/service/pokeapi/functions/fetchPokemonList.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+
+import { BASE_URL, SPRITES_BASE_URL } from '../constants';
+
+async function fetchPokemonList({ offset = 20, limit = 20, nextUrl = null }) {
+  try {
+    let url = `${BASE_URL}/pokemon/`;
+
+    const config = {
+      params: {
+        offset,
+        limit,
+      },
+    };
+
+    if (next) {
+      url = nextUrl;
+    }
+
+    const response = await axios.get(url, config);
+
+    // get front_default image from sprites
+    const resultWithSprite = response.data.results.map(({ name, url }) => {
+      const id = url.slice(url.length - 2, -1);
+      const sprite = `${SPRITES_BASE_URL}/pokemon/${id}.png`;
+
+      return { name, url, sprite };
+    });
+
+    return {
+      ...response.data,
+      results: resultWithSprite,
+    };
+  } catch (error) {
+    return error.response;
+  }
+}
+
+export default fetchPokemonList;
